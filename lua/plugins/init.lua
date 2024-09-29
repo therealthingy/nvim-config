32de6d33e15696de32c053eb3fac30397369ea76
Gerrit Klein <therealthingy95@gmail.com>
2024-09-29 13:02:25 +0200

    chore: treesitter + style: indent

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,25 +1,43 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+    {
+        "stevearc/conform.nvim",
+        -- event = 'BufWritePre', -- uncomment for format on save
+        opts = require "configs.conform",
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+    -- These are some examples, uncomment them if you want to see them work!
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end,
+    },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        -- DOC: TSInstallInfo,
+        opts = {
+            ensure_installed = {
+                "csv",
+                "awk",
+                "gnuplot",
+                "diff", "passwd", "udev", "tmux",
+                "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore",
+                "latex",
+                "python",
+                "c", "cpp", "cmake", "doxygen",
+                "rust",
+                "nasm", "llvm",
+                -- "disassebmly",
+                "objdump", "strace",
+                "lua", "luadoc",
+                "ninja",
+                "dockerfile",
+                "toml",
+                "yaml",
+                "markdown",
+                "http", "html", "css"
+            },
+        },
+    },
 }
